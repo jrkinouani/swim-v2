747f744c210cd699927a72b389cf9d2529146de9
jrkinouani <kinouani@gmail.com>
2018-10-18 06:21:40 +0400

    hotfix

---
+++ app/controllers/aquabikes_controller.rb
@@ -0,0 +1,38 @@
+class AquabikesController < ApllicationController
+  before_action :set_aquabike, only: [:show, :update, :destroy, :edit]
+
+  def new
+    @aquabike = Aquabike.new
+  end
+  def create
+    @aquabike = Aquabike.create(aquabike_params)
+    redirect_to aquabike_path(@aquabike)
+  end
+  def show
+  end
+
+  def update
+    @aquabike.update(aquabike_params)
+    rediredct_to aquabike_path(@aquabike)
+  end
+
+  def index
+    @aquabikes = Aquabike.all
+  end
+  def edit
+
+  end
+  def destroy
+  end
+
+  private
+
+  def aquabike_params
+    params.require(:aquabike).permit(:title, :price)
+  end
+
+  def set_aquabike
+    @aquabike = Aquabike.find(params[:id])
+  end
+
+end
