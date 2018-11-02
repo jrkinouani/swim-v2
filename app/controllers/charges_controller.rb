d2e630bb9ea0026c20014063bc9aad3dc7ff5a9a
jrkinouani <kinouani@gmail.com>
2018-11-02 17:30:56 +0400

    add reservationgym charges

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,14 +1,21 @@
 class ChargesController < ApplicationController
 
   def new
+    if params[:reservation_id]
       @reservation = Reservation.find(params[:reservation_id])
+    else
+      @reservationgym = Reservationgym.find(params[:reservationgym_id])
+    end
   end
 
   def create
+    if params[:reservation_id]
      @reservation = Reservation.find(params["reservation_id"])
-     # Amount in cents
      @amount = @reservation.price.to_i * 100
-
+    else
+     @reservationgym = Reservationgym.find(params["reservationgym_id"])
+     @amount = @reservationgym.price.to_i * 100
+    end
          customer = Stripe::Customer.create(
        :email => params[:stripeEmail],
        :source  => params[:stripeToken]
@@ -20,7 +27,7 @@ class ChargesController < ApplicationController
        :description => 'Rails Stripe customer',
        :currency    => 'eur'
      )
-     @reservation = Reservation.find(params["reservation_id"])
+     #@reservation = Reservation.find(params["reservation_id"])
       #Notifier.paiement(@reservation).deliver
 
     rescue Stripe::CardError => e
