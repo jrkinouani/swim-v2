03d7fd0def9eddb344a3bf9a0fdc5bfbf6a8529c
jrkinouani <kinouani@gmail.com>
2018-11-09 15:17:17 +0400

    delete page pages

--- config/routes.rb
+++ config/routes.rb
@@ -64,17 +64,8 @@ Rails.application.routes.draw do
 
   resources :charges
 
-  get 'pages/aquabike' , to: "pages#aquabike"
-  get 'pages/aquaboxing' , to: "pages#aquaboxing"
-  get 'pages/aquaparcours' , to: "pages#aquaparcours"
-  get '/ecole-natation-petit' , to: "pages#petit"
-  get 'pages/fitness' , to: "pages#fitness"
-  get 'ecole-natation-moyen', to: "pages#moyen"
-  get 'ecole-natation-adulte', to: "pages#adulte"
-  get 'pages/bebe' , to: "pages#bebe"
+
   get 'pages/mention' , to: "pages#mention"
-  get 'jardin-aquatique' , to: "pages#jardin"
-  get 'pages/staticswims' , to: "pages#staticswims"
   get 'pages/apropos', to: "pages#apropos"
 
   root :to => "pages#home"
