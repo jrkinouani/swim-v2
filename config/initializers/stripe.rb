e22be6f8d6a610b6267ac44f07d637dbb00e5607
jrkinouani <kinouani@gmail.com>
2018-11-13 15:27:09 +0400

    fix key stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['Publishable_key'],
+  :secret_key      => ENV['Secret_key']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
