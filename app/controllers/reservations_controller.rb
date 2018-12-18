64884490a5459d44f9d803c8c58a120e273e8415
jrkinouani <kinouani@gmail.com>
2018-12-18 08:01:45 +0100

    add limit reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -27,18 +27,23 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.create!(reservation_params)
+    @reservation = Reservation.create(reservation_params)
+    if @reservation.errors.count > 0
+        flash[:danger] = @reservation.errors.full_messages.join(',')
+      redirect_to new_reservation_path(aquabike_id: @reservation.aquabike_id)
+    else
      aquabike = @reservation.aquabike
-     if aquabike
-       price = aquabike.price
-       if price
-        @reservation.price = aquabike.price
-      else
-          flash[:danger] = 'Vous avez mal renseigné les champs de textes !'
-      end
-      @reservation.save
+       if aquabike
+         price = aquabike.price
+         if price
+          @reservation.price = aquabike.price
+        else
+            flash[:danger] = 'Vous avez mal renseigné les champs de textes !'
+        end
+        @reservation.save
+       end
+       redirect_to reservation_path(@reservation)
      end
-     redirect_to reservation_path(@reservation)
   end
 
   def reservation_params
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,43 @@
 class Reservation < ApplicationRecord
   belongs_to :aquabike
+  validate :limit_personne
+
+  def self.list_creneau
+    ["selectionnez un créneau",
+       "Lundi: 12h15 - 13h",
+       "Lundi: 17h30 - 18h15",
+      "Mardi: 12h15 - 13h",
+      "Mardi: 18h15 - 19h00",
+      "Mercredi: 12h15 - 13h",
+      "Mrecredi: 17h30 - 18h15",
+      "Mercredi:18H15 - 19h",
+      "Jeudi: 12h15 - 13h",
+      "Jeudi: 17h30 - 18h15",
+      "Jeudi: 1!h15 - 19h00",
+       "Vendredi: 12h15 - 13h",
+      "Samedi: 12h15 - 13h"]
+  end
+
+  def self.creneau_limit
+    {
+      "Lundi: 12h15 - 13h" => 6,
+      "Lundi: 17h30 - 18h15"=> 6,
+     "Mardi: 12h15 - 13h" => 6,
+     "Mardi: 18h15 - 19h00" => 6,
+     "Mercredi: 12h15 - 13h" => 6,
+     "Mrecredi: 17h30 - 18h15" => 6,
+     "Mercredi:18H15 - 19h" => 6,
+     "Jeudi: 12h15 - 13h" => 6,
+     "Jeudi: 17h30 - 18h15" => 6,
+     "Jeudi: 1!h15 - 19h00" => 6,
+      "Vendredi: 12h15 - 13h" => 6,
+     "Samedi: 12h15 - 13h" => 6,
+    }
+  end
+
+  def limit_personne
+    if Reservation.creneau_limit[creneau] ==  Reservation.where(creneau: creneau).count
+      errors.add(:creneau, "complet, choisissez un autre créneau")
+    end
+  end
 end
